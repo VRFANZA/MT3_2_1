c42594d97959adfe6dcbfb07252ab4c957d45be6
VRFANZA <k024g0085@g.neec.ac.jp>
2025-05-21 14:08:57 +0900

    変な判定修正

--- main.cpp
+++ main.cpp
@@ -161,6 +161,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		}
 
 		ImGui::Begin("Sphere s1 Controller");
+		ImGui::SetWindowSize(ImVec2(400, 300)); // 幅400, 高さ300（お好みで変更）
 		ImGui::SliderFloat3("s1.center", &s1.center.x, -5.0f, 5.0f);
 		ImGui::SliderFloat("s1.radius", &s1.radius, 0.1f, 5.0f);
 		ImGui::End();
@@ -289,7 +290,15 @@ Vector3 Add(const Vector3& v1, const Vector3& v2)
 
 bool IsCollision(const Sphere& s1, const Sphere& s2)
 {
-	float distance = GetLength(s2.center) - GetLength(s1.center);
+	// 中心間ベクトルを計算
+	Vector3 delta = {
+		s2.center.x - s1.center.x,
+		s2.center.y - s1.center.y,
+		s2.center.z - s1.center.z
+	};
+
+	// ２つの中心間距離
+	float distance = GetLength(delta);
 
 	if (distance <= s1.radius + s2.radius) {
 		return true;
